897cc153e777b1219579ecb591640e54a360a82f
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-05-23 15:41:52 -0700

    Fixed sql commands in seed file

--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,3 @@
-ALTER TABLE exercise CONSTRAINT fk_exercise_list FOREIGN KEY(list_id) REFERENCES exercise_list(list_id);
-ALTER TABLE workout_plans CONSTRAINT fk_workout FOREIGN KEY(workout_id) REFERENCES workout(workout_id);
-ALTER TABLE exercise_list CONSTRAINT fk_exercise FOREIGN KEY(exercise_id) REFERENCES exercise(exercise_id);
+ALTER TABLE exercise ADD CONSTRAINT fk_exercise_list FOREIGN KEY(list_id) REFERENCES exercise_list(list_id);
+ALTER TABLE workout_plans ADD CONSTRAINT fk_workout FOREIGN KEY(workout_id) REFERENCES workout(workout_id);
+ALTER TABLE exercise_list ADD CONSTRAINT fk_exercise FOREIGN KEY(exercise_id) REFERENCES exercise(exercise_id);
