d42084243ac1cbb00a895d6407983d73a212f03e
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-05-23 16:29:29 -0700

    Installed pg, nodemon and tested my initial get routes

--- server.js
+++ server.js
@@ -10,10 +10,55 @@ const pool = new Pool({
     user: 'postgres',
     port: 5432,
     password: 'marine',
-    database: mvp_project
+    database: 'mvp_project'
 })
 
 app.use(express.json());
+
+app.get("/api/calendar", async (req,res) => {
+    try {
+        const data = await pool.query('SELECT * FROM calendar;')
+        res.send(data.rows);
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+app.get("/api/workout", async (req,res) => {
+    try {
+        const data = await pool.query('SELECT * FROM workout;')
+        res.send(data.rows);
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+app.get("/api/exercise", async (req,res) => {
+    try {
+        const data = await pool.query('SELECT * FROM exercise;')
+        res.send(data.rows);
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+app.get("/api/workout_plans", async (req,res) => {
+    try {
+        const data = await pool.query('SELECT * FROM workout_plans;')
+        res.send(data.rows);
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+app.get("/api/exercise_list", async (req,res) => {
+    try {
+        const data = await pool.query('SELECT * FROM exercise_list;')
+        res.send(data.rows);
+    } catch (err) {
+        console.error(err.message)
+    }
+})
 
 app.listen(port, () => {
     console.log(`Running on ${port}`)
