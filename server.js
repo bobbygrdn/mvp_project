8179fe563af0719fae7e8288689d79202ab1acd5
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-05-23 15:15:20 -0700

    Finished setup of express server no routes

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
 const express = require('express');
 const app = express();
 
+const { Pool } = require('pg');
+
+const port = process.env.PORT || 4000;
+
+const pool = new Pool({
+    host: 'localhost',
+    user: 'postgres',
+    port: 5432,
+    password: 'marine',
+    database: mvp_project
+})
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Running on ${port}`)
+})
