25aa75e08c9c21fccb5333014a4a64776b594d5f
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-05-24 16:39:21 -0700

    Added more styling

--- public/app.js
+++ public/app.js
@@ -18,7 +18,7 @@ const buttonDev = () => {
         user.style.display = "none";
         currentplan.style.display = "none";
         workouts.style.display= "none";
-        history.style.display="inline-flex";
+        history.style.display="block";
         const calendarurl = 'https://desolate-reef-75349.herokuapp.com/api/calendar/';
     let calendarhtml = '';
     fetch(calendarurl)
