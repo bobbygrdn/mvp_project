a8d7c67614a946fcbdc04419789508ec2e7afb4a
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-05-25 13:29:47 -0700

    Created basic queries and functions

---
+++ public/app.js
@@ -0,0 +1,28 @@
+//Buttons
+const calendar = document.querySelector('.calendar');
+const list = document.querySelector('.list');
+const profile = document.querySelector('.profile');
+
+//Forms
+const history = document.querySelector('#history_input');
+const exercise = document.querySelector('#exercise_input');
+const workout = document.querySelector('#workout_input');
+
+const startUp = () => {
+
+}
+
+//Form Helper Functions
+const historyForm = () => {
+    history.style.display = 'none'
+}
+
+const exerciseForm = () => {
+    exercise.style.display = 'none'
+}
+
+const workoutForm = () => [
+    workout.style.display = 'none'
+]
+
+startUp();
