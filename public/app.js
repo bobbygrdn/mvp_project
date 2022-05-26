5b09232e75f1cbf585e8cb587776f268ad20764a
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-05-26 09:32:41 -0700

    targeting divs

--- public/app.js
+++ public/app.js
@@ -136,7 +136,7 @@ const createListItem = (elem) => {
     div.className = 'workout_items'
     div.style.cursor = 'pointer'
     div.addEventListener('click', (e) => [
-        console.log(e.currentTarget)
+        console.log(e.currentTarget.id)
         // changeWorkoutForm()
     ])
     workouts.appendChild(div)
