0f9b0028c233a5a3a34026a0e33ac7a35445da1c
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-05-26 09:25:46 -0700

    Updated event targeting

--- public/app.js
+++ public/app.js
@@ -136,7 +136,7 @@ const createListItem = (elem) => {
     div.className = 'workout_items'
     div.style.cursor = 'pointer'
     div.addEventListener('click', (e) => [
-        console.log(e.composedPath)
+        console.log(e.composedPath())
         // changeWorkoutForm()
     ])
     workouts.appendChild(div)
